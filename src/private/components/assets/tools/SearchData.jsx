0e7ba42964445c959003a924957e1970e0949faa
Myke William Oliveira <maikewilliam.dev@gmail.com>
2024-09-10 09:52:30 -0300

    FEAT add function calc price cart

--- src/private/components/assets/tools/SearchData.jsx
+++ src/private/components/assets/tools/SearchData.jsx
@@ -14,24 +14,24 @@ function SearchData({searchConfig, setQuerySearch, searchFunction}) {
 
   return (
     <div className="util-box">
-      <div className="util-func">
-        <label htmlFor="search">Campo de busca</label>
-        <input type="text" name="search" id="search" onChange={(e) => setQuery(e.target.value)}/>  
-        <button className='bt-primary' onClick={searchFunction} >
-          <MdOutlineContentPasteSearch/>
-        </button>
-      </div>  
-      <div className='util-func'>
-        <label htmlFor="search_by">Forma de busca</label>
-        <select name="search_by" id="search_by" onChange={(e) => {setSearchType(e.target.value)}}>
-          {
-          search.options.map((item,index) => (
-              <option key={index+item.value} value={item.value}>{item.txt}</option>
-            )) 
-          }
-        </select>
-      </div>
-  </div>
+        <div className="util-func">
+          <label htmlFor="search">Campo de busca</label>
+          <input type="text" name="search" id="search" onChange={(e) => setQuery(e.target.value)}/>  
+          <button className='bt-primary' onClick={searchFunction} >
+            <MdOutlineContentPasteSearch/>
+          </button>
+        </div>  
+        <div className='util-func'>
+          <label htmlFor="search_by">Forma de busca</label>
+          <select name="search_by" id="search_by" onChange={(e) => {setSearchType(e.target.value)}}>
+            {
+            search.options.map((item,index) => (
+                <option key={index+item.value} value={item.value}>{item.txt}</option>
+              )) 
+            }
+          </select>
+        </div>
+    </div>
   )
 }
 
--- src/private/components/cart/register/ShowCart.jsx
+++ src/private/components/cart/register/ShowCart.jsx
@@ -1,10 +1,12 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { CartContext } from '../../../context/CartProvider'
 import WrapperProgress from '../assets/WrapperProgress';
 import { FaTrashAlt } from "react-icons/fa";
 
 function ShowCart() {
   const { productsData, setProductsData } = useContext(CartContext);
+  const [ cartPrice, setCartPrice ] =useState(0);
+  const [ qtdProducts, setQtdProducts ] = useState(0);
 
   const handdleCartRemove = (item) => {
     const newArrProd = productsData.map( product => {
@@ -18,12 +20,42 @@ function ShowCart() {
     setProductsData(newArrProd);
   }
 
+  useEffect(()=>{
+
+    let price = productsData.reduce((acc, item) => (
+      item.inCart ? Number(item.price) * Number(item.qtd_products) : 0
+    )+acc,0)
+
+    let formatingPrice = price.toLocaleString( 'pt-br',{ style:"currency", currency:"BRL"})
+    setCartPrice(formatingPrice);
+
+    let qtd = productsData.reduce((acc, item) => (
+      item.inCart ? Number(item.qtd_products) : 0
+    ) + acc,0);
+    setQtdProducts(qtd);
+
+  },[productsData])
+
   return (
     <div className='module-content'>
       <div className="top-utils">
         <WrapperProgress/>
       </div>
       <div className="utils-content">
+
+        <div className="util-box">
+          <div className="util-func">
+            <label htmlFor="search">Valor carrinho</label>
+            <input type="text" name="search" className='txt-center' id="search" readOnly value={cartPrice} />  
+          </div>  
+          <div className='util-func'>
+            <label htmlFor="search_by">Qtd Produtos</label>
+            <input type="number" name="search" className='txt-center' id="search" readOnly value={qtdProducts}/>  
+          </div>
+        </div>
+
+        <button className='bt bt-clean'>Limpar carrinho</button>
+
       </div>
       <div className="module-actions">
         <div className="content-table">
