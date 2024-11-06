89eb451fcb54466bd99d4f3718ada5048d1c0a16
myke william oliveira feitosa <maikewilliam.dev@gmail.com>
2024-11-06 00:20:31 -0300

    FEAT add component handdler pricing product

--- src/private/components/products/assets/ProdFieldRigth.jsx
+++ src/private/components/products/assets/ProdFieldRigth.jsx
@@ -2,15 +2,20 @@ import React from 'react'
 import './ProdFieldRigth.css'
 import ProdCreateBrand from './ProdCreateBrand'
 import ProdCreateCategory from './ProdCreateCategory'
+import ProdPriccing from './ProdPriccing'
 
 function ProdFieldRigth() {
 
+
   return (
     <div className='prod_rigth_fields'>
+      
       <div className="attributes_create">
         <ProdCreateBrand/>
         <ProdCreateCategory/>
       </div>
+      <ProdPriccing/>
+
     </div>
   )
 }
---
+++ src/private/components/products/assets/ProdPriccing.jsx
@@ -0,0 +1,103 @@
+import React, { useEffect, useState } from 'react'
+
+function ProdPriccing() {
+  const [productCost, setProductCost] = useState('');
+  const [feelsTaxes, setFeelsTaxes ] = useState(0);
+  const [discounts, setDiscounts] = useState(0);
+  const [profitMargin,setProfitMargin] = useState(0);
+  const [sellingPrice, setSellingPrice]= useState('');
+
+  const formatingPrice = (e) => {
+    let value = e.target.value;
+
+    // Remove qualquer caractere não numérico
+    value = value.replace(/\D/g, '');
+
+    // Formata o valor para moeda BRL adicionando duas casas decimais
+    const formattedPrice = new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+      minimumFractionDigits: 2
+    }).format(value / 100);
+
+    return formattedPrice
+  }
+
+  function unformatPrice(formattedPrice) {
+    // Remova o símbolo de moeda e os pontos, substitua vírgula por ponto
+    const numericValue = formattedPrice
+      .replace(/[R$\s.]/g, '') // Remova "R$", espaços e pontos
+      .replace(',', '.'); // Substitui vírgula por ponto e converta em float
+    
+    return parseFloat(numericValue);
+  }
+
+  const productCoastHanddler = (e) => {
+    let value = formatingPrice(e);
+    setProductCost(value);
+  }
+
+  const sellingPriceHandle = () => {
+    let coastProduct = unformatPrice(productCost);
+    let appliProfitMargin =  (coastProduct + (coastProduct * (profitMargin / 100))).toFixed(2);
+    let appliFeelsTaxes = (appliProfitMargin - (appliProfitMargin * (feelsTaxes / 100))).toFixed(2);
+    let appliDiscounts = (appliFeelsTaxes - (appliFeelsTaxes * (discounts /100))).toFixed(2);
+
+    setSellingPrice(appliDiscounts);
+  }
+
+  useEffect(() => {
+    if(productCost !== '' || feelsTaxes > 0 || discounts > 0 || profitMargin > 0){
+      sellingPriceHandle();
+    }
+  }, [productCost,feelsTaxes,discounts,profitMargin])
+
+
+
+
+  return (
+    <div className="attributes_pricing">
+      <div className="fd-group">
+        <label htmlFor="product_cost">Preço de custo</label>
+        <input 
+          type="text" 
+          id='product_cost' 
+          name='product_cost' 
+          className='text_align-end' 
+          value={productCost}
+          onChange={productCoastHanddler} 
+        />
+      </div>
+
+      <div className="fd-group">
+        <label htmlFor="fees_and_taxes">Taxas/impostos %</label>
+        <input type="text" id='fees_and_taxes' name='fees_and_taxes' className='text_align-center' onChange={(e) => setFeelsTaxes(e.target.value)}  />
+      </div>
+      
+      <div className="fd-group">
+        <label htmlFor="profit_margin">Margem/Lucro %</label>
+        <input type="text" id='profit_margin' name='profit_margin' className='text_align-center' onChange={(e) => setProfitMargin(e.target.value)} />
+      </div>
+      <div className="fd-group ">
+        <label htmlFor="discounts">Descontos %</label>
+        <input type="text" id='discounts' name='discounts' className='text_align-center' onChange={(e) => setDiscounts(e.target.value)}/>
+      </div>
+      <div className="fd-group ">
+        <label htmlFor="stock">Estoque</label>
+        <input type="text" id='stock' name='stock' className='text_align-center'/>
+      </div>
+      <div className="fd-group text_align">
+        <label htmlFor="currency_id">Moeda</label>
+        <select name="currency_id" id="currency_id" className='text_align-center'>
+          <option value="BRL">Real</option>
+        </select>
+      </div>
+      <div className="fd-group">
+        <label htmlFor="selling_price">Preço de venda</label>
+        <input type="text" id='selling_price' name='selling_price' className='text_align-end' value={sellingPrice} />
+      </div>
+    </div>
+  )
+}
+
+export default ProdPriccing
