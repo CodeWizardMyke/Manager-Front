a20341491386ef1895c453b24f00f9af5902f332
Myke William Oliveira Feitosa <maikewilliam.dev@gmail.com>
2026-01-25 13:20:43 -0300

    FEAT add new function for create a new user in auth page

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-
-### `npm install`
-
-### `npm start`
+* ajustar um popUp de mensagem para usuarios que acessarem a plataforma e nao estiverem com um token valido, informar e retornalos para a tela de login em seguida
--- src/private/UserAuth.jsx
+++ src/private/UserAuth.jsx
@@ -8,6 +8,7 @@ import Loading from './components/loading/Loading';
 function UserAuth() {
   const [keepLogIn, setKeepLogIn] = useState('off');
   const [loading, setLoading] = useState(false);
+  const [createAcc, setCreateAcc] = useState(false);
   const navigate = useNavigate();
 
   function cleanStorage(){
@@ -30,24 +31,42 @@ function UserAuth() {
     navigate('/main');
   }
 
+  async function createNewUser(formData){
+    return await fetchAxios.post('/employee/crud/create', formData);
+  }
+
+  async function loginUser(formData){
+    return await fetchAxios.post('/auth/login', formData);
+  }
+
   async function handdlerSubmit (event) {
     try {
       event.preventDefault();
       setLoading(true);
       updateErrorsSpan();
-      
       const formData = new FormData(event.target);
-      const response = await fetchAxios.post('/auth/login', formData);
-      userAthorized(response);
-      console.log('response', response)
 
+      let response = null;
+
+      if(createAcc === true){
+        await createNewUser(formData);
+        window.alert('Usuário cadastrado com sucesso, faça o login para validar sua sessão.')
+      }
+      if(createAcc === false){
+        response = await loginUser(formData)
+      }
+      if(response !== null){
+        userAthorized(response);
+      }
+      console.log('response', response);
+      setLoading(false);
     } catch (error) {
       setLoading(false);
       console.log(error);
       if(error.response){
         handdlerErrors(error.response.data);
       };
-    };
+    }      
   }
 
   function handdlerErrors(data){
@@ -70,24 +89,55 @@ function UserAuth() {
         <div className="content-auth">
           <div className="label-auth"><span>Login de usuarios</span></div>
           <form onSubmit={(e) => handdlerSubmit(e) }>
+            {
+              createAcc && (
+                <>
+                  <div>
+                    <label htmlFor="userName">Nome</label>
+                    <input type="text" name="name" id="userName" required/>
+                  </div>
 
+                  <div>
+                    <label htmlFor="role">Cargo</label>
+                    <select name="role" id="role">
+                      <option value="aux">Auxiliar</option>
+                      <option value="adimin">Administrador</option>
+                    </select>
+                  </div>
+                </>
+              )
+            }
             <div>
-              <label htmlFor="email">Login</label>
+              <label htmlFor="email">Email</label>
               <input type="email" name="email" id="email" required/>
             </div>
-
+           
             <div>
               <label htmlFor="password">Senha</label>
               <input type="password" name="password" id="password" required/>
             </div>
+            {
+              createAcc && (
+                <div>
+                  <label htmlFor="re_password">Senha</label>
+                  <input type="password" name="re_password" id="re_password" required/>
+                </div>
+              )
+            }
 
-            <article className='keep-login'>
-              <label htmlFor="keeplogin">manter credenciais</label>
-              <input type="checkbox" name="keeplogin" id="keeplogin" onChange={(e) => setKeepLogIn(e.target.value)}/>
+            <article className='suggestions'>
+              <div>
+                <label htmlFor="keeplogin">manter credenciais</label>
+                <input type="checkbox" name="keeplogin" id="keeplogin" onChange={(e) => setKeepLogIn(e.target.value)}/>
+              </div>
+              <div>
+                <input type="button" value={ !createAcc ? "Fazer novo cadastro" : "Conta existente fazer login"} className='singup' onClick={()=> setCreateAcc(!createAcc)} />
+              </div>
             </article>
-              <div className="content-errros"></div>
+
+            <div className="content-errros"></div>
             <article className="submit-auth">
-                <button type="submit">Entrar</button>
+                <button type="submit">{ createAcc ? "Enviar cadastro" : "Entrar" }</button>
             </article>
           </form>
         </div>
